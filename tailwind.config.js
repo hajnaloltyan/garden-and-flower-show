c75f5e826db365df6d8eb6f7cfa944bf8b4c30b1
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-04-19 00:05:18 +0200

    Add background images and update styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ['./docs/*.{html,js}'],
   theme: {
+    fontFamily: {
+      sans: ['Lato', 'sans-serif'],
+    },
+    backgroundImage: {
+      'hero-image': 'url("./images/light3.jpg")',
+      'main-image': 'url("./images/green-main.jpg")',
+    },
     extend: {},
   },
   plugins: [],
